5210bbe7662eb7aa30eae9938d453d3383c40ae9
webzhuce07 <lajiwebzhuce@163.com>
2018-04-30 22:12:05 +0800

    add logo
    add logo

---
+++ part2/ROIExample/mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "utility.h"
+#include <QFileDialog>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_openImageButton_clicked()
+{
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), ".", tr("Image Files(*.png *jpg *bmp)"));
+    image = cv::imread(fileName.toStdString());
+    QImage img = cvMat2QImage(image);
+    ui->displayLabel->setPixmap(QPixmap::fromImage(img));
+
+}
+
+void MainWindow::on_processButton_clicked()
+{
+    if(!image.data)
+        return;
+    QString fileName = QFileDialog::getOpenFileName(this, tr("Open Image"), ".", tr("Image Files(*.png *jpg *bmp)"));
+    cv::Mat logo = cv::imread(fileName.toStdString());
+    if(!logo.data)
+        return;
+    cv::Mat result = image.clone();
+    int y = (image.rows > logo.rows) ? (image.rows - logo.rows) : 0;
+    int x = (image.cols > logo.cols) ? (image.cols - logo.cols) : 0;
+    int height = (image.rows > logo.rows) ? logo.rows : image.rows;
+    int width = (image.cols > logo.cols) ? logo.cols : image.cols;
+    cv::Mat imageROI =  result(cv::Rect(x,y ,width, height));
+    //insert logo
+    cv::addWeighted(imageROI, 1.0, logo, 0.3, 0.0, imageROI);
+    QImage img = cvMat2QImage(result);
+    ui->displayLabel->setPixmap(QPixmap::fromImage(img));
+}
+
